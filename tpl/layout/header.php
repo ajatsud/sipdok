185e5de11a2c113a1b2f34d177d2a78b9a435509
Ajat <ajatsudrajat.s.kom@gmail.com>
2024-01-05 13:03:05 +0700

    change font

--- tpl/layout/header.php
+++ tpl/layout/header.php
@@ -15,7 +15,7 @@ if (!defined("VER")) {
 	<title><?= $title ?></title>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 	<link href="/res/style.css?version=<?= filemtime(DIR . SEP . "res" . SEP .  'style.css') ?>" rel="stylesheet">
 </head>
 
